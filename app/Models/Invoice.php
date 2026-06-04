fcec4f5fb18317919917538249c7ea449c97e891
Dipo Kusumo <m.alam@students.paramadina.ac.id>
2026-06-04 17:33:31 +0700

    feat: importer invoice done

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -66,9 +66,9 @@ class Invoice extends Model
 
             $factor = (float) $pphType->factor;
 
-            $taxRate = (float) $invoice->pphType->tax_rate;
+            $taxRate = (float) $pphType->tax_rate;
 
-            $isGrossUp = (bool) $invoice->pphType->is_gross_up;
+            $isGrossUp = (bool) $pphType->is_gross_up;
 
             $pphAmount = $invoice->base_amount / $factor * $taxRate / 100;
 
